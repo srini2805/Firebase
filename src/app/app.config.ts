f423326f5a3dd143d0616beb71b286ee8a3ef49d
srini2805 <ssrinithi@student.tce.edu>
2024-07-19 12:00:51 +0530

    Complete Firebase Setup and configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -19,6 +19,6 @@ export const appConfig: ApplicationConfig = {
       provideStorage(() => getStorage()),
       provideMessaging(() => getMessaging())
     ),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-1bd9a","appId":"1:1023842058348:web:3cb589ee4bde132443d849","storageBucket":"friendlychat-1bd9a.appspot.com","locationId":"asia-south1","apiKey":"AIzaSyBfywxScuPGC9ty2X9d_E_GlezP6gVcK00","authDomain":"friendlychat-1bd9a.firebaseapp.com","messagingSenderId":"1023842058348"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
